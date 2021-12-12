6cba8f982e33a84407532363401ac2ef29568ea5
Chad Whitt <youhavechad@icloud.com>
2021-12-12 17:49:52 -0600

    Exercise 2 Extra Credit 3. Custom Hook for local storage.

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,16 +3,23 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-
-  const [name, setName] = React.useState(
-    () => window.localStorage.getItem('name') ?? initialName,
+function useLocalStorageState(key, defaultValue = '') {
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem(key) ?? defaultValue,
   )
 
   React.useEffect(() => {
-    window.localStorage.setItem('name', name)
-  }, [name])
+    window.localStorage.setItem(key, state)
+  }, [key, state])
+
+  return [state, setState]
+}
+
+function Greeting({initialName = ''}) {
+  // 🐨 initialize the state to the value from localStorage
+
+  const [name, setName] = useLocalStorageState('name', initialName)
+
   function handleChange(event) {
     setName(event.target.value)
   }
